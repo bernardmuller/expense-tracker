899ccdbd58ed36e482d687fc0278107d8139ce76
Bernard <b.mullerjnr@gmail.com>
2025-08-23 15:35:02 +0200

    feat: netlify

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart({
       customViteReactPlugin: true,
-      target: 'cloudflare-module'
+      target: 'netlify'
     }),
     viteReact(),
   ],
