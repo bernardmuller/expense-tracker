13d3c53bf6a6cc4e045aab380a1089e5ef8b76b0
Bernard <b.mullerjnr@gmail.com>
2025-08-23 17:16:18 +0200

    feat: add new manifest file

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -66,6 +66,10 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="en" className="dark">
       <head>
         <HeadContent />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="dark">
         {children}
