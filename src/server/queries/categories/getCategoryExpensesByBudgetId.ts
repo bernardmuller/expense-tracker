ed280e9ede83cf47fe5f4e8e3bd3b11c7cdcacd5
Bernard <b.mullerjnr@gmail.com>
2025-09-24 01:04:23 +0200

    feat: add category expenses query

---
+++ src/server/queries/categories/getCategoryExpensesByBudgetId.ts
@@ -0,0 +1,35 @@
+import { eq, sql, sum } from "drizzle-orm"
+import { db } from "@/db"
+import { categories, categoryBudgets, expenses, userCategories } from "@/db/schema"
+
+export default async function getCategoryExpensesByBudgetId(id: number) {
+	const subquery = db
+		.select({
+			key: expenses.category,
+			spent: sum(expenses.amount).as('spent'),
+			budgetId: expenses.budgetId
+		})
+		.from(expenses)
+		.where(eq(expenses.budgetId, id))
+		.groupBy(expenses.category, expenses.budgetId)
+		.as('spent')
+
+	const result = await db
+		.select({
+			id: categories.id,
+			key: categories.key,
+			name: categories.label,
+			icon: categories.icon,
+			spent: sql`coalesce(${subquery.spent}, 0)`.as('spent'), // Using sql template
+			planned: categoryBudgets.allocatedAmount
+		})
+		.from(userCategories)
+		.innerJoin(categories, eq(userCategories.categoryId, categories.id))
+		.innerJoin(categoryBudgets, eq(categories.id, categoryBudgets.categoryId))
+		.leftJoin(subquery, eq(subquery.key, categories.key)) // Changed to leftJoin
+		.groupBy(categories.key, categoryBudgets.allocatedAmount, categories.label, categories.icon, categories.id, subquery.spent)
+
+	return result;
+}
+
+export type CategoryExpensesByBudgetId = Awaited<ReturnType<typeof getCategoryExpensesByBudgetId>>
