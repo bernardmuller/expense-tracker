63a07f4119969b46d8755f5353bf1d1a58c54c5d
Bernard <b.mullerjnr@gmail.com>
2025-09-25 18:58:51 +0200

    feat: add nitro to test coolify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -12,9 +13,10 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart({
-      customViteReactPlugin: true,
-      target: 'netlify'
+      // customViteReactPlugin: true,
+      // target: 'netlify'
     }),
+    nitro(),
     viteReact(),
   ],
   build: {
