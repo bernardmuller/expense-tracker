4602356506e394c39a37b4ba00dc1991d0367ebd
Bernard <b.mullerjnr@gmail.com>
2025-10-01 21:45:19 +0200

    feat: add user mock data with tests

---
+++ apps/api/src/domain/entities/__mocks__/user.mock.ts
@@ -0,0 +1,14 @@
+import type { User } from '../user';
+import { faker } from '@faker-js/faker';
+
+export const generateMockUser = (): User => ({
+  id: faker.string.uuid(),
+  email: faker.internet.email(),
+  onboarded: faker.datatype.boolean(),
+  emailVerified: faker.datatype.boolean(),
+  name: faker.person.firstName(),
+});
+
+export const mockUsers = (count: number = 10): User[] => {
+  return Array.from({ length: count }, () => generateMockUser());
+};
