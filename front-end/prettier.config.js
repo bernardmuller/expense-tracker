cccc995d5a54226f08bf9a8d4db9efbaf6430762
sins <sinsmailza@gmail.com>
2025-10-03 21:55:02 +0200

    chore: add prettier plugins

--- front-end/prettier.config.js
+++ front-end/prettier.config.js
@@ -2,9 +2,14 @@
 
 /** @type {import('prettier').Config} */
 const config = {
+  plugins: [
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-classnames',
+    'prettier-plugin-merge',
+  ],
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  trailingComma: 'all',
+}
 
-export default config;
+export default config
