64d58068f8c80438018d9e8954fba5d65744bf9d
sins <sinsmailza@gmail.com>
2025-10-04 01:15:33 +0200

    chore: use generator in storybook

--- front-end/src/components/budget-breakdowns/BugetBreakdowns.stories.tsx
+++ front-end/src/components/budget-breakdowns/BugetBreakdowns.stories.tsx
@@ -2,6 +2,7 @@ import { fn } from 'storybook/test'
 
 import BudgetBreakdownItem from '@/components/budget-breakdowns/BudgetBreakdownItem'
 import type { Meta, StoryObj } from '@storybook/react-vite'
+import { generateBudgetBreakdownItemProps } from './__mocks__/budgetBreakDownItemProps.mock'
 
 const meta = {
   title: 'Form/BudgetBreakdownItem',
@@ -13,15 +14,16 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+const budgetBreakdownItemProps = generateBudgetBreakdownItemProps()
+
 export const Primary: Story = {
   args: {
-    icon: '🫡',
-    isOverBudget: true,
-    isUnplanned: false,
-    key: 'key',
-    name: 'name',
-    percentage: 5,
-    spent: 500,
-    planned: 5,
+    icon: budgetBreakdownItemProps.icon,
+    isOverBudget: budgetBreakdownItemProps.isOverBudget,
+    isUnplanned: budgetBreakdownItemProps.isUnplanned,
+    name: budgetBreakdownItemProps.name,
+    percentage: budgetBreakdownItemProps.percentage,
+    spent: budgetBreakdownItemProps.spent,
+    planned: budgetBreakdownItemProps.planned,
   },
 }
