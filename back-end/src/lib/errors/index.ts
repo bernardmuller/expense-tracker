ce8d5e0d58440578d8388cc39cf1ce6e737abbdf
Bernard <b.mullerjnr@gmail.com>
2025-10-03 22:27:13 +0200

    feat: add soft delete transaction

--- back-end/src/lib/errors/index.ts
+++ back-end/src/lib/errors/index.ts
@@ -4,6 +4,10 @@ export class UserAlreadyDeletedError extends Data.TaggedError(
   'UserAlreadyDeletedError'
 )<{}> { }
 
+export class AlreadyDeletedError extends Data.TaggedError(
+  'AlreadyDeletedError'
+)<{}> { }
+
 export class CreateTransactionError extends Data.TaggedError('CreateTransactionError')<{}> { }
 
 export class UpdateTransactionError extends Data.TaggedError('UpdateTransactionError')<{
