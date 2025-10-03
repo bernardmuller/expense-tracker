3b6c5d1170bdf5bf8d0d3e6dd8d33da65c771130
sins <sinsmailza@gmail.com>
2025-10-04 01:14:19 +0200

    chore: add random int and random bool utils

--- front-end/src/lib/utils.ts
+++ front-end/src/lib/utils.ts
@@ -5,3 +5,11 @@ import type {ClassValue} from 'clsx';
 export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
 }
+
+export function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+export function getRandomBool() {
+  return Math.random() < 0.5
+}
