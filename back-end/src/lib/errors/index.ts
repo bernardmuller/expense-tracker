e07bcfc1f8a7e12c9a804c68bff4801f809a3a8f
Bernard <b.mullerjnr@gmail.com>
2025-10-04 13:50:17 +0200

    feat: create budget tests

--- back-end/src/lib/errors/index.ts
+++ back-end/src/lib/errors/index.ts
@@ -1,14 +1,15 @@
 import { Data } from 'effect';
 
-export class UserAlreadyDeletedError extends Data.TaggedError(
-  'UserAlreadyDeletedError'
-)<{}> { }
+export class ValidationError extends Data.TaggedError('ValidationError')<{
+  message: string;
+}> { }
 
-export class AlreadyDeletedError extends Data.TaggedError(
-  'AlreadyDeletedError'
-)<{}> { }
+export class UserAlreadyDeletedError extends Data.TaggedError('UserAlreadyDeletedError')<{}> { }
+
+export class AlreadyDeletedError extends Data.TaggedError('AlreadyDeletedError')<{}> { }
 
 export class CreateTransactionError extends Data.TaggedError('CreateTransactionError')<{}> { }
 
-export class UpdateTransactionError extends Data.TaggedError('UpdateTransactionError')<{
-}> { }
+export class UpdateTransactionError extends Data.TaggedError('UpdateTransactionError')<{}> { }
+
+export class CreateBudgetError extends Data.TaggedError('CreateBudgetError')<{}> { }
