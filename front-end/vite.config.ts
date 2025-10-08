60d1cf16bd82bba6597c326518d225930470bc9a
sins <sinsmailza@gmail.com>
2025-10-08 21:12:35 +0200

    chore: add pwa support

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { VitePWA } from 'vite-plugin-pwa'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
+    VitePWA({ registerType: 'autoUpdate' }),
     viteReact(),
     tailwindcss(),
   ],
