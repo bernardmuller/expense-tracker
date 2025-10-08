ded4bb9e658b94755af23959abc42903d0762595
sins <sinsmailza@gmail.com>
2025-10-08 20:49:59 +0200

    chore: add open api doc generation with scalar UI

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,15 +1,66 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import {
+  describeRoute,
+  resolver,
+  openAPIRouteHandler,
+  validator,
+} from "hono-openapi";
+import { Schema as S } from "effect";
+import { Scalar } from "@scalar/hono-api-reference";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const querySchema = S.Struct({
+  query: S.String,
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const responseSchema = S.String;
+const responseStandardSchema = S.standardSchemaV1(responseSchema);
+const queryStandardSchema = S.standardSchemaV1(querySchema);
+
+app.get(
+  "/healthcheck",
+  describeRoute({
+    description: "return a query as a response",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "text/plain": { schema: resolver(responseStandardSchema) },
+        },
+      },
+    },
+  }),
+  validator("query", queryStandardSchema),
+  (c) => {
+    const query = c.req.valid("query");
+    return c.json(`Your query: ${query.query}`);
+  },
+);
+
+app.get(
+  "/docs",
+  openAPIRouteHandler(app, {
+    documentation: {
+      info: {
+        title: "Hono API",
+        version: "1.0.0",
+        description: "Greeting API",
+      },
+      servers: [{ url: "http://localhost:3000", description: "Local Server" }],
+    },
+  }),
+);
+
+app.get("/scalar", Scalar({ url: "/docs" }));
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
