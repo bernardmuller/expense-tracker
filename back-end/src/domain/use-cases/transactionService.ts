62df77d18b133ac6d27f5bf5fde2ff2358f526a8
Bernard <b.mullerjnr@gmail.com>
2025-10-09 16:17:32 +0200

    wip

--- back-end/src/domain/use-cases/transactionService.ts
+++ back-end/src/domain/use-cases/transactionService.ts
@@ -1,18 +1,24 @@
-import { Context, Effect } from "effect";
 import type {
   CreateTransactionParams,
   Transaction,
 } from "@/domain/entities/transaction";
 import type { TransactionError } from "@/domain/entities/transaction/transactionErrors";
+import { Context, Effect } from "effect";
 
 export interface TransactionServiceShape {
   readonly createTransaction: (
     params: CreateTransactionParams,
-  ) => Effect.Effect<Transaction, TransactionError>;
+  ) => Effect.Effect<Transaction, TransactionError | Error>;
   readonly updateTransaction: (
     transaction: Transaction,
     params: Transaction,
-  ) => Effect.Effect<Transaction, TransactionError>;
+  ) => Effect.Effect<Transaction, TransactionError | Error>;
+  readonly isExpenseTransaction: (
+    transaction: Transaction,
+  ) => Effect.Effect<boolean, never>;
+  readonly isIncomeTransaction: (
+    transaction: Transaction,
+  ) => Effect.Effect<boolean, never>;
 }
 
 export class TransactionService extends Context.Tag(
