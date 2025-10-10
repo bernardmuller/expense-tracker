876ff515008f510b1431865ca42f759b08f6abc8
sins <sinsmailza@gmail.com>
2025-10-10 15:08:11 +0200

    wip

---
+++ front-end/src/components/budget-breakdowns/UnplannedBudgetBreakdownItem.test.tsx
@@ -0,0 +1,33 @@
+import { screen, render } from '@testing-library/react'
+import { generateBudgetBreakdownItemProps } from './__mocks__/budgetBreakDownItemProps.mock'
+import UnplannedBudgetBreakdownItem from './UnplannedBudgetBreakdownItem'
+
+describe('UnplannedBudgetBreakdownItem', () => {
+  const { name, icon, spentAmount } = generateBudgetBreakdownItemProps({
+    spentAmount: 'R1 253',
+  })
+  beforeEach(() => {
+    render(
+      <UnplannedBudgetBreakdownItem
+        icon={icon}
+        name={name}
+        spentAmount={spentAmount}
+      />,
+    )
+  })
+  it('should render "unplanned"', () => {
+    expect(screen.getByText(/unplanned/i)).toBeInTheDocument()
+  })
+  it('should render "spent"', () => {
+    expect(screen.getByText(/spent/i)).toBeInTheDocument()
+  })
+  it('should render the spent amount', () => {
+    expect(screen.getByText(spentAmount)).toBeInTheDocument()
+  })
+  it('should render the icon', () => {
+    expect(screen.getByText(icon)).toBeInTheDocument()
+  })
+  it('should render the name', () => {
+    expect(screen.getByText(name)).toBeInTheDocument()
+  })
+})
