0717bdf4496cd207915bf87fe71b27165622a329
Bernard <b.mullerjnr@gmail.com>
2025-10-10 14:04:17 +0200

    feat: add user layer

--- back-end/src/domain/repositories/transactionRepository.ts
+++ back-end/src/domain/repositories/transactionRepository.ts
@@ -1,16 +1,9 @@
 import { Context, Effect } from "effect";
 import type { Transaction } from "@/domain/entities/transaction";
+import type { BaseRepositoryShape } from "./baseRepository";
 
-export interface TransactionRepositoryShape {
-  readonly create: (
-    transaction: Transaction,
-  ) => Effect.Effect<Transaction, Error>;
-  readonly findById: (id: string) => Effect.Effect<Transaction | null, Error>;
-  readonly update: (
-    transaction: Transaction,
-  ) => Effect.Effect<Transaction, never>;
-  readonly delete: (transaction: Transaction) => Effect.Effect<boolean, never>;
-}
+export interface TransactionRepositoryShape
+  extends BaseRepositoryShape<Transaction, Error, never, Error, never> {}
 
 export class TransactionRepository extends Context.Tag(
   "domain/repositories/transactionRepository",
