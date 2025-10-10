df215b82125342c0d09a98f7fc9f230470b1f966
Bernard <b.mullerjnr@gmail.com>
2025-10-10 10:49:44 +0200

    feat: budget layer

--- back-end/src/application/use-cases/budgetService.ts
+++ back-end/src/application/use-cases/budgetService.ts
@@ -1,29 +1,53 @@
-import { Context, Effect, Layer } from "effect";
+import { Context, Effect, Layer, pipe } from "effect";
 import type { CreateBudgetParams, Budget } from "@/domain/entities/budget";
-import * as budgetEntity from "@/domain/entities/budget";
+import * as BudgetEntity from "@/domain/entities/budget";
 import { BudgetRepository } from "@/domain/repositories/budgetRepository";
 import { BudgetService } from "@/domain/use-cases/budgetService";
 
 export const BudgetServiceLive = Layer.effect(
   BudgetService,
-  Effect.gen(function* () {
-    const budgetRepository = yield* BudgetRepository;
-
-    return {
-      createbudget: (params: CreatebudgetParams) =>
-        Effect.gen(function* () {
-          const budget = yield* budgetService.createbudget(params);
-          return yield* budgetRepository.create(budget);
-        }),
-
-      updatebudget: (budget: budget, params: budget) =>
-        Effect.gen(function* () {
-          const updatedbudget = yield* budgetEntity.updatebudget(
-            budget,
-            params,
-          );
-          return yield* budgetRepository.create(updatedbudget);
-        }),
-    };
-  }),
+  pipe(
+    BudgetRepository,
+    Effect.map((budgetRepository) => ({
+      createBudget: (params: CreateBudgetParams) =>
+        pipe(
+          BudgetEntity.createBudget(params),
+          Effect.andThen(budgetRepository.create),
+        ),
+      getBudgetById: (id: string) => budgetRepository.findById(id),
+      getBudgetSpentAmount: (budget: Budget) =>
+        BudgetEntity.getBudgetSpentAmount(budget),
+      getBudgetSpentPercentage: (budget: Budget) =>
+        BudgetEntity.getBudgetSpentPercentage(budget),
+      setBudgetActive: (budget: Budget) =>
+        pipe(
+          BudgetEntity.setBudgetActive(budget),
+          Effect.andThen(budgetRepository.update),
+        ),
+      setBudgetInactive: (budget: Budget) =>
+        pipe(
+          BudgetEntity.setBudgetInactive(budget),
+          Effect.andThen(budgetRepository.update),
+        ),
+      isBudgetActive: (budget: Budget) =>
+        BudgetEntity.isBudgetActive(budget),
+      isBudgetOverbudget: (budget: Budget) =>
+        BudgetEntity.isBudgetOverbudget(budget),
+      updateBudgetName: (budget: Budget, params: Budget) =>
+        pipe(
+          BudgetEntity.updateBudgetName(budget, params.name),
+          Effect.andThen(budgetRepository.update),
+        ),
+      addToBudgetCurrentAmount: (budget: Budget, amount: number) =>
+        pipe(
+          BudgetEntity.addToBudgetCurrentAmount(budget, amount),
+          Effect.andThen(budgetRepository.update),
+        ),
+      subtractFromBudgetCurrentAmount: (budget: Budget, amount: number) =>
+        pipe(
+          BudgetEntity.subtractFromBudgetCurrentAmount(budget, amount),
+          Effect.andThen(budgetRepository.update),
+        ),
+    })),
+  ),
 );
