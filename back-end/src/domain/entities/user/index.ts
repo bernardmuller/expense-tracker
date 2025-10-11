e482e03cc0d42657845bc1aba0679984d9b1c8b9
Bernard <b.mullerjnr@gmail.com>
2025-10-11 16:02:15 +0200

    refactor: user entity business logic to use neverthrow

--- back-end/src/domain/entities/user/index.ts
+++ back-end/src/domain/entities/user/index.ts
@@ -1,11 +1,10 @@
-import { Effect } from "effect";
 import {
   type UserValidationError,
   UserAlreadyOnboardedError,
   UserAlreadyVerifiedError,
 } from "./userErrors";
 import { generateUuid } from "@/lib/utils/generateUuid";
-import { ok } from "neverthrow";
+import { err, ok } from "neverthrow";
 
 export type User = {
   readonly id: string;
@@ -18,53 +17,51 @@ export type User = {
 export type CreateUserParams = Omit<User, "id" | "emailVerified" | "onboarded">;
 
 export const createUser = (params: CreateUserParams) => {
-    const uuid = generateUuid();
-    return ok({
-      id: uuid,
-      ...params,
-      emailVerified: false,
-      onboarded: false,
-    });
+  const uuid = generateUuid();
+  return ok({
+    id: uuid,
+    ...params,
+    emailVerified: false,
+    onboarded: false,
+  });
 };
 
-export const markUserAsOnboarded = (user: User) =>
-  Effect.gen(function* () {
-    if (user.onboarded) {
-      return yield* Effect.fail(
-        new UserAlreadyOnboardedError({
-          userId: user.id,
-        }),
-      );
+export const markUserAsOnboarded = (user: User) => {
+  if (user.onboarded) {
+    return err(
+      new UserAlreadyOnboardedError({
+        userId: user.id,
+      }),
+    );
+  }
 
-    return {
-      ...user,
-      onboarded: true,
-    } satisfies User;
+  return ok({
+    ...user,
+    onboarded: true,
+  } satisfies User);
+};
+
+export const markUserAsVerified = (user: User) => {
+  if (user.emailVerified) {
+    return err(
+      new UserAlreadyVerifiedError({
+        userId: user.id,
+      }),
+    );
+  }
+
+  return ok({
+    ...user,
+    emailVerified: true,
   });
+};
 
-export const markUserAsVerified = (user: User) =>
-  Effect.gen(function* () {
-    if (user.emailVerified) {
-      return yield* Effect.fail(
-        new UserAlreadyVerifiedError({
-     userId: user.id,
-        }),
-      );
-    }
-
-    return {
-      ...user,
-      emailVerified: true,
-    };
+export const updateUser = (user: User, updatedUser: User) => {
+  return ok({
+    ...user,
+    name: updatedUser.name,
   });
-
-export const updateUser = (user: User, updatedUser: User) =>
-  Effect.gen(function* () {
-    return {
-      ...user,
-      name: updatedUser.name,
-    };
-  });
+};
 
 export const isUserFullySetup = (user: User) =>
   user.onboarded && user.emailVerified;
