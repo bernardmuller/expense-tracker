6f13b25c61a611d6c1065295a82863a99084f1a6
Bernard <b.mullerjnr@gmail.com>
2025-10-11 16:05:45 +0200

    refactor: decimal place util function with error class

--- back-end/src/lib/utils/getNumberOfDecimalPlaces.ts
+++ back-end/src/lib/utils/getNumberOfDecimalPlaces.ts
@@ -1,12 +1,16 @@
-import { Data, Effect } from "effect";
+import { err, ok } from "neverthrow";
 
 const floatRegex = /^-?\d+(\.\d+)?$/;
 
-export class InvalidDecimalNumberString extends Data.TaggedError(
-  "InvalidDecimalNumberString",
-)<{}> {}
+export class InvalidDecimalNumberString extends Error {
+  readonly _tag = "InvalidDecimalNumberString";
+  constructor() {
+    super("Invalid decimal number string");
+    this.name = "InvalidDecimalNumberString";
+  }
+}
 
 export const getNumberOfDecimalPlaces = (value: string) =>
   floatRegex.test(value)
-    ? Effect.succeed(value.split(".")[1]?.length ?? 0)
-    : Effect.fail(new InvalidDecimalNumberString());
+    ? ok(value.split(".")[1]?.length ?? 0)
+    : err(new InvalidDecimalNumberString());
