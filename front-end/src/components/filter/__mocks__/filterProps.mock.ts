c35bc2be445ef2bf82313ef79c86b79d5a42f8d0
sins <sinsmailza@gmail.com>
2025-10-14 19:25:26 +0200

    feat: add filter component

--- front-end/src/components/filter/__mocks__/filterProps.mock.ts
+++ front-end/src/components/filter/__mocks__/filterProps.mock.ts
@@ -1,9 +1,7 @@
-import type { FilterProps } from "./Filter.types"
-import { filterProps } from "./filterProps.factory"
+import type { FilterProps } from './../Filter.types'
+import { filterProps } from './filterProps.factory'
 
-export const generateFilterProps = (
-  overwrites: Partial<FilterProps> = {},
-) => ({
+export const generateFilterProps = (overwrites: Partial<FilterProps> = {}) => ({
   ...filterProps,
-  ...overwrites
+  ...overwrites,
 })
