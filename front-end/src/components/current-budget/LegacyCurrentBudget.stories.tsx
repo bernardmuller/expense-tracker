81bcd5f684fce1b17d070b1f42456dce7ae41f5b
sins <sinsmailza@gmail.com>
2025-10-15 21:17:08 +0200

    chore/add-legacy-component-story

---
+++ front-end/src/components/current-budget/LegacyCurrentBudget.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { LegacyCurrentBudget } from './LegacyCurrentBudget'
+
+const meta = {
+  title: 'Legacy Current Budget',
+  component: LegacyCurrentBudget,
+} satisfies Meta<typeof LegacyCurrentBudget>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
