b486d7189613d63b3640a159acc252997e2f8a0a
sins <sinsmailza@gmail.com>
2025-10-18 23:57:47 +0200

    feat: spinner component

--- front-end/src/components/spinner-button/SpinnerButton.tsx
+++ front-end/src/components/spinner-button/SpinnerButton.tsx
@@ -3,6 +3,7 @@ import { Spinner } from '@/components/ui/spinner'
 
 type SpinnerButtonProps = {
   enabledText: string
+<<<<<<< HEAD
   onClick?: () => void
   className?: string
   buttonProps?: React.ComponentProps<'button'>
@@ -13,12 +14,23 @@ type SpinnerButtonProps = {
   | {
       isLoading?: boolean
       isDisabled: true
+=======
+  onClick: () => void
+  className?: string
+} & (
+  | {
+      disabled?: false
+    }
+  | {
+      disabled: true
+>>>>>>> 4bedd2e (feat: spinner component)
       disabledText: string
     }
 )
 
 export function SpinnerButton(props: SpinnerButtonProps) {
   const { enabledText, onClick, className } = props
+<<<<<<< HEAD
   const text = props.isDisabled ? props.disabledText : enabledText
 
   return (
@@ -30,6 +42,18 @@ export function SpinnerButton(props: SpinnerButtonProps) {
       {...props.buttonProps}
     >
       {props.isDisabled && props.isLoading && <Spinner />}
+=======
+  const text = props.disabled ? props.disabledText : enabledText
+
+  return (
+    <Button
+      disabled={props.disabled}
+      size="sm"
+      onClick={onClick}
+      className={className}
+    >
+      {props.disabled && <Spinner />}
+>>>>>>> 4bedd2e (feat: spinner component)
       {text}
     </Button>
   )
