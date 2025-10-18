a66cddb264de5aa7c9040e4bb91cbd0da1f5a3a8
sins <sinsmailza@gmail.com>
2025-10-18 13:41:20 +0200

    chore: format filter components

--- front-end/src/components/filter/Filter.types.ts
+++ front-end/src/components/filter/Filter.types.ts
@@ -1,10 +1,10 @@
 import type { HTMLProps } from 'react'
 
 export type FilterProps = {
-  filterItems: {
+  filterItems: Array<{
     name: string
     value: string
-  }[]
+  }>
   placeHolder: string
   handleValueChange: (value: string) => void
   rootClassName?: HTMLProps<HTMLElement>['className']
