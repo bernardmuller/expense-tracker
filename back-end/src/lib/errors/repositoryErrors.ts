18cc06480f0c5224c0010e181de4283180178c59
Bernard <b.mullerjnr@gmail.com>
2025-10-20 08:54:30 +0200

    feat: error handling refinements

--- back-end/src/lib/errors/repositoryErrors.ts
+++ back-end/src/lib/errors/repositoryErrors.ts
@@ -3,26 +3,51 @@ import { createError } from "@/lib/utils/createError";
 export const EntityNotFoundError = createError(
   "EntityNotFoundError",
   (entity: string) => `${entity} not found`,
+  {
+    code: "ENTITY_NOT_FOUND",
+    error: "Not Found",
+    statusCode: 404,
+  },
 );
 
 export const EntityCreateError = createError(
   "EntityCreateError",
   (entity: string) => `Failed to create ${entity}`,
+  {
+    code: "ENTITY_CREATE_ERROR",
+    error: "Failed to Create",
+    statusCode: 500,
+  },
 );
 
 export const EntityUpdateError = createError(
   "EntityUpdateError",
   (entity: string) => `Failed to update ${entity}`,
+  {
+    code: "ENTITY_UPDATE_ERROR",
+    error: "Failed to Update",
+    statusCode: 500,
+  },
 );
 
 export const EntityDeleteError = createError(
   "EntityDeleteError",
   (entity: string) => `Failed to delete ${entity}`,
+  {
+    code: "ENTITY_DELETE_ERROR",
+    error: "Failed to Delete",
+    statusCode: 500,
+  },
 );
 
 export const EntityReadError = createError(
   "EntityReadError",
   (entity: string) => `Failed to read ${entity}`,
+  {
+    code: "ENTITY_READ_ERROR",
+    error: "Failed to Read",
+    statusCode: 500,
+  },
 );
 
 export type RepositoryErrorType =
