141ff37d048d30916ad6f36f6a5a51472c6c8b9a
sins <sinsmailza@gmail.com>
2025-10-21 21:52:44 +0200

    chore: add is loading state

--- front-end/src/components/spinner-button/SpinnerButton.tsx
+++ front-end/src/components/spinner-button/SpinnerButton.tsx
@@ -3,7 +3,6 @@ import { Spinner } from '@/components/ui/spinner'
 
 type SpinnerButtonProps = {
   enabledText: string
-<<<<<<< HEAD
   onClick?: () => void
   className?: string
   buttonProps?: React.ComponentProps<'button'>
@@ -12,25 +11,16 @@ type SpinnerButtonProps = {
       isDisabled?: false
     }
   | {
+      disabledText: string
       isLoading?: boolean
       isDisabled: true
-=======
-  onClick: () => void
-  className?: string
-} & (
-  | {
-      disabled?: false
-    }
-  | {
-      disabled: true
->>>>>>> 4bedd2e (feat: spinner component)
-      disabledText: string
+      onClick: () => void
+      className?: string
     }
 )
 
 export function SpinnerButton(props: SpinnerButtonProps) {
   const { enabledText, onClick, className } = props
-<<<<<<< HEAD
   const text = props.isDisabled ? props.disabledText : enabledText
 
   return (
@@ -39,21 +29,8 @@ export function SpinnerButton(props: SpinnerButtonProps) {
       size="sm"
       onClick={onClick}
       className={className}
-      {...props.buttonProps}
     >
       {props.isDisabled && props.isLoading && <Spinner />}
-=======
-  const text = props.disabled ? props.disabledText : enabledText
-
-  return (
-    <Button
-      disabled={props.disabled}
-      size="sm"
-      onClick={onClick}
-      className={className}
-    >
-      {props.disabled && <Spinner />}
->>>>>>> 4bedd2e (feat: spinner component)
       {text}
     </Button>
   )
