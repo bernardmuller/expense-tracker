52c96c10ba5cdebf49b2890f2b65d95110513b05
sins <sinsmailza@gmail.com>
2025-10-25 21:05:24 +0200

    chore: add radix test dependant configuration

--- front-end/vitestSetup.ts
+++ front-end/vitestSetup.ts
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom/vitest'
+
+//Mocking Radix Elements (I LOVE TESTING!!!!!!!)
+window.HTMLElement.prototype.scrollIntoView = vi.fn() //https://stackoverflow.com/questions/53271193/typeerror-scrollintoview-is-not-a-function
+window.HTMLElement.prototype.hasPointerCapture = vi.fn() //https://github.com/testing-library/user-event/discussions/1087
