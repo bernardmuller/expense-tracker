25ade3a70b7b6cf301201163d55c0b5623e051a7
sins <sinsmailza@gmail.com>
2025-10-25 21:07:50 +0200

    chore: add base add expense form tests

--- front-end/src/components/add-expense-form/__tests__/AddExpenseForm.test.tsx
+++ front-end/src/components/add-expense-form/__tests__/AddExpenseForm.test.tsx
@@ -1,3 +1,4 @@
+import setupUserEvent from '@/lib/utils/testing/setupUserEvent'
 import { render, screen } from '@testing-library/react'
 import AddExpenseForm from '../AddExpenseForm'
 
@@ -21,5 +22,25 @@ describe('AddExpenseForm', () => {
   it('should render a button', () => {
     render(<AddExpenseForm onSubmit={vi.fn()} />)
     expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+  it('should show all warnings when button is pressed with no inputs entered', async () => {
+    const { user } = setupUserEvent(<AddExpenseForm onSubmit={vi.fn()} />)
+    const submitButton = screen.getByRole('button')
+    await user.click(submitButton)
+    expect(
+      screen.getByText('You must provide a description'),
+    ).toBeInTheDocument()
+    expect(screen.getByText('You must provide the amount')).toBeInTheDocument()
+    expect(screen.getByText('You must specify a category')).toBeInTheDocument()
+  })
+  it('should call a function on submit', async () => {
+    const mockFunction = vi.fn()
+    const { user } = setupUserEvent(<AddExpenseForm onSubmit={mockFunction} />)
+    await user.type(screen.getByPlaceholderText('Expense name'), 'Mackers')
+    await user.type(screen.getByPlaceholderText('Amount'), '1000')
+    await user.click(screen.getByRole('combobox'))
+    await user.click(screen.getAllByText('Housing')[1])
+    await user.click(screen.getByRole('button'))
+    expect(mockFunction).toHaveBeenCalled()
   })
 })
