3877f83f310d93aa6c4976f7a1e29aaa6815422a
sins <sinsmailza@gmail.com>
2025-11-02 20:18:43 +0200

    chore: remove unnecessary conditional

--- front-end/src/components/recent-expenses/RecentExpense.tsx
+++ front-end/src/components/recent-expenses/RecentExpense.tsx
@@ -11,14 +11,12 @@ export default function RecentExpense({
         last:border-b-0"
     >
       <div className="flex items-center gap-3">
-        {emoji && (
-          <span
-            className="flex h-6 w-6 items-center justify-center rounded-full
-              text-lg"
-          >
-            {emoji}
-          </span>
-        )}
+        <span
+          className="flex h-6 w-6 items-center justify-center rounded-full
+            text-lg"
+        >
+          {emoji}
+        </span>
         <span className="font-medium">{description}</span>
       </div>
       <span className="font-semibold">{amount}</span>
