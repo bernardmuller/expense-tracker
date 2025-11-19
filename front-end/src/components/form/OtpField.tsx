d1dfd0a9960faa7a084986db550147988c7b739f
Bernard <b.mullerjnr@gmail.com>
2025-11-19 08:17:32 +0200

    feat: add opt form component

---
+++ front-end/src/components/form/OtpField.tsx
@@ -0,0 +1,40 @@
+import { useFieldContext } from '@/hooks/form-context'
+import { useStore } from '@tanstack/react-form'
+import { Field, FieldError, FieldLabel } from '../ui/field'
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from '../ui/input-otp'
+
+export default function OtpField({
+  label,
+}: {
+  label?: string
+}) {
+  const field = useFieldContext<string>()
+  const isInvalid = useStore(field.store, (state) => !state.meta.isValid)
+
+  return (
+    <Field data-invalid={isInvalid} className="items-center">
+      {label && <FieldLabel htmlFor={field.name}>{label}</FieldLabel>}
+      <InputOTP
+        id={field.name}
+        maxLength={6}
+        value={field.state.value}
+        onChange={(value) => field.handleChange(value)}
+        onBlur={field.handleBlur}
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+      {isInvalid && <FieldError errors={field.state.meta.errors} />}
+    </Field>
+  )
+}
