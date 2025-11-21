0732161d729b8131190a1e212fe610345a6e8634
sins <sinsmailza@gmail.com>
2025-11-21 20:05:32 +0200

    chore: update style and move listener to form declaration

--- front-end/src/components/form/OtpField.tsx
+++ front-end/src/components/form/OtpField.tsx
@@ -1,34 +1,35 @@
-import { useFieldContext } from '@/hooks/form-context'
-import { useStore } from '@tanstack/react-form'
 import { Field, FieldError, FieldLabel } from '@/components/ui/field'
 import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSeparator,
-  InputOTPSlot,
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSlot
 } from '@/components/ui/input-otp'
+import { useFieldContext } from '@/hooks/form-context'
+import { useStore } from '@tanstack/react-form'
 
 export default function OtpField({ label }: { label?: string }) {
   const field = useFieldContext<string>()
   const isInvalid = useStore(field.store, (state) => !state.meta.isValid)
 
   return (
-    <Field data-invalid={isInvalid} className='bg-black-900'>
-      {label && <FieldLabel htmlFor={field.name}>{label}</FieldLabel>}
+    <Field data-invalid={isInvalid}>
+      {label && (
+        <FieldLabel className="flex justify-center" htmlFor={field.name}>
+          {label}
+        </FieldLabel>
+      )}
       <InputOTP
         id={field.name}
         maxLength={6}
         value={field.state.value}
         onChange={(value) => field.handleChange(value)}
         onBlur={field.handleBlur}
+        className="flex items-center"
       >
         <InputOTPGroup>
           <InputOTPSlot index={0} />
           <InputOTPSlot index={1} />
           <InputOTPSlot index={2} />
-        </InputOTPGroup>
-        <InputOTPSeparator />
-        <InputOTPGroup>
           <InputOTPSlot index={3} />
           <InputOTPSlot index={4} />
           <InputOTPSlot index={5} />
