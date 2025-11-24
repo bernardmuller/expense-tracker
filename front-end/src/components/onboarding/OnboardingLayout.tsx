37fdcb470e231e4b652932f5c8586422e042af0f
Bernard <b.mullerjnr@gmail.com>
2025-11-24 11:35:05 +0200

    feat: refine page styling, put StepHeader on Card

--- front-end/src/components/onboarding/OnboardingLayout.tsx
+++ front-end/src/components/onboarding/OnboardingLayout.tsx
@@ -5,7 +5,7 @@ type OnboardingLayoutProps = {
 export default function OnboardingLayout({ children }: OnboardingLayoutProps) {
   return (
     <div className="flex h-screen items-stretch justify-center p-4">
-      <div className="flex w-full max-w-2xl flex-col pt-20">{children}</div>
+      <div className="flex w-full max-w-2xl flex-col pt-10">{children}</div>
     </div>
   )
 }
