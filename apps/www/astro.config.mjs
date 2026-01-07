5d8d7e55e4684732c650e579197396f26b9f18bd
Bernard <b.mullerjnr@gmail.com>
2026-01-07 15:51:44 +0200

    feat: netlify deployment for www

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,20 +1,21 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': '/src'
-      }
-    }
+        "@": "/src",
+      },
+    },
   },
 
   integrations: [react()],
+  adapter: netlify(),
 });
