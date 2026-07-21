bdc775b57ce315010b40f8a4745cb2f5cfe7c4ea
Bernard <b.mullerjnr@gmail.com>
2026-07-21 17:41:18 +0200

    chore: property shutdown bot server

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,13 +1,22 @@
 import { serve } from "@hono/node-server";
 import app from "./app";
 import env from "./env";
-import "./lib/telegram";
+import { bot } from "./lib/telegram";
 
 const port = env.PORT;
 // eslint-disable-next-line no-console
 console.log(`Server is running on http://localhost:${port}`);
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port,
 });
+
+const shutdown = () => {
+  bot.stopPolling().finally(() => {
+    server.close(() => process.exit(0));
+  });
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
