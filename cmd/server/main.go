735d74f0b2e2564fdbe427b24f3f0ff7d1d5f3b2
Bernard <b.mullerjnr@gmail.com>
2026-08-14 00:07:56 +0200

    refactor: better app, config and handler setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	"database/sql"
@@ -17,22 +18,30 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+type Config struct {
+	DatabaseUrl      string
+	TelegramBotToken string
+}
+
+type App struct {
+	Config Config
+	DB     *postgres.Queries
+}
+
+type MessageHandler struct {
+	DB *postgres.Queries
+}
+
+func NewMessageHandler(db *postgres.Queries) *MessageHandler {
+	return &MessageHandler{
+		DB: db,
+	}
+}
+
+func (h MessageHandler) onMessage(ctx context.Context, b *bot.Bot, update *models.Update) {
 	if update.Message.Text == "/users" {
-		uri := os.Getenv("DATABASE_URL")
-		if uri == "" {
-			log.Fatal("failed to load database url")
-		}
 
-		db, err := sql.Open("postgres", uri)
-		if err != nil {
-			log.Fatal("failed to connect to database : %w", err)
-		}
-
-		ctx := context.Background()
-		queries := postgres.New(db)
-
-		users, err := queries.GetAllUsers(ctx)
+		users, err := h.DB.GetAllUsers(ctx)
 		if err != nil {
 			log.Fatalf("failed to fetch users: %v", err)
 		}
@@ -46,30 +55,61 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 			Text:   strings.Join(names, ", "),
 		})
 	}
-
 }
 
-func main() {
+func CreateConfig() (*Config, error) {
 	if os.Getenv("ENV") != "production" {
 		err := godotenv.Load()
 		if err != nil {
-			log.Fatal("failed to load env: %w", err)
+			return nil, err
 		}
 	}
 
+	cfg := Config{
+		DatabaseUrl:      os.Getenv("DATABASE_URL"),
+		TelegramBotToken: os.Getenv("TELEGRAM_BOT_TOKEN"),
+	}
+
+	if cfg.DatabaseUrl == "" || cfg.TelegramBotToken == "" {
+		return nil, errors.New("missing env vars")
+	}
+
+	return &cfg, nil
+}
+
+func CreateApp() (*App, error) {
+	cfg, err := CreateConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := sql.Open("postgres", cfg.DatabaseUrl)
+	if err != nil {
+		log.Fatal("failed to connect to database : %w", err)
+	}
+	db := postgres.New(conn)
+
+	return &App{
+		Config: *cfg,
+		DB:     db,
+	}, nil
+}
+
+func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
+	app, err := CreateApp()
+	if err != nil {
+		log.Fatal(err)
+	}
+	h := NewMessageHandler(app.DB)
+
 	opts := []bot.Option{
-		bot.WithDefaultHandler(handler),
+		bot.WithDefaultHandler(h.onMessage),
 	}
 
-	token := os.Getenv("TELEGRAM_BOT_TOKEN")
-	if token == "" {
-		log.Fatal("failed to load telegram token")
-	}
-
-	b, err := bot.New(token, opts...)
+	b, err := bot.New(app.Config.TelegramBotToken, opts...)
 	if err != nil {
 		panic(err)
 	}
