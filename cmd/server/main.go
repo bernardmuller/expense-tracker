cbcc2108f15c8764634e06dfaea24357396628ed
Bernard <b.mullerjnr@gmail.com>
2026-08-13 23:31:21 +0200

    feat: basic bot to db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,43 +2,51 @@ package main
 
 import (
 	"context"
+	"strings"
+
 	"database/sql"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"os/signal"
 
 	"expenny.co.za/internal/store/postgres"
 	"github.com/joho/godotenv"
 
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	_ "github.com/lib/pq"
 )
 
-func getAllUsers(w http.ResponseWriter, req *http.Request) {
-	uri := os.Getenv("DATABASE_URL")
-	if uri == "" {
-		log.Fatal("failed to load database url")
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	if update.Message.Text == "/users" {
+		uri := os.Getenv("DATABASE_URL")
+		if uri == "" {
+			log.Fatal("failed to load database url")
+		}
+
+		db, err := sql.Open("postgres", uri)
+		if err != nil {
+			log.Fatal("failed to connect to database : %w", err)
+		}
+
+		ctx := context.Background()
+		queries := postgres.New(db)
+
+		users, err := queries.GetAllUsers(ctx)
+		if err != nil {
+			log.Fatalf("failed to fetch users: %v", err)
+		}
+
+		names := make([]string, 0, len(users))
+		for _, u := range users {
+			names = append(names, u.Name)
+		}
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   strings.Join(names, ", "),
+		})
 	}
 
-	db, err := sql.Open("postgres", uri)
-	if err != nil {
-		log.Fatal("failed to connect to database : %w", err)
-	}
-
-	ctx := context.Background()
-	queries := postgres.New(db)
-
-	users, err := queries.GetAllUsers(ctx)
-	if err != nil {
-		log.Fatalf("failed to fetch users: %v", err)
-	}
-
-	names := make([]string, 0, len(users))
-	for _, u := range users {
-		names = append(names, u.Name)
-	}
-
-	fmt.Printf("names: %v", names)
 }
 
 func main() {
@@ -49,7 +57,22 @@ func main() {
 		}
 	}
 
-	http.HandleFunc("/users", getAllUsers)
-	fmt.Println("on port 8080")
-	http.ListenAndServe(":8080", nil)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if token == "" {
+		log.Fatal("failed to load telegram token")
+	}
+
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
 }
